514ebf44cdc9da1427141541375abd62bcd8d1b5
Zachary Kirby <i.am.kirbynator@gmail.com>
2020-01-31 14:42:22 -0700

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :articles
     get "search/:query", :to => "articles#search"
       end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      get '*other', to: 'static#index'
 end
